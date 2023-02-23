3c09caee48061b2e2872d4d439c188c8706fd3c4
Trin4317 <eg.artourbabaev@gmail.com>
2023-02-23 14:42:51 +0000

    update customer api test for index method

--- tests/Feature/CustomerApiTest.php
+++ tests/Feature/CustomerApiTest.php
@@ -14,11 +14,25 @@ class CustomerApiTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_user_can_access_api(): void
+    public function test_user_can_get_all_customers(): void
     {
+        $customers = Customer::factory(20)->create();
+
         $response = $this->getJson('/api/v1/customers');
 
-        $response->assertStatus(200);
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->has('meta')
+                     ->has('links')
+                     ->has('data', 10)
+                     ->has('data.0', fn (AssertableJson $json) =>
+                        $json->where('id', $customers[0]->id)
+                             ->where('name', $customers[0]->name)
+                             ->where('postalCode', $customers[0]->postal_code)
+                             ->etc()
+                )
+            );
     }
 
     public function test_user_can_get_specific_customer(): void
