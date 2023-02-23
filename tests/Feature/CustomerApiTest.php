159caba67de8ad3736de15358d48489eb0efd847
Trin4317 <eg.artourbabaev@gmail.com>
2023-02-23 14:01:31 +0000

    update customer api test for show method

--- tests/Feature/CustomerApiTest.php
+++ tests/Feature/CustomerApiTest.php
@@ -4,8 +4,10 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Models\Customer;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class CustomerApiTest extends TestCase
@@ -17,5 +19,21 @@ class CustomerApiTest extends TestCase
         $response = $this->getJson('/api/v1/customers');
 
         $response->assertStatus(200);
+    }
+
+    public function test_user_can_get_specific_customer(): void
+    {
+        $customer = Customer::factory()->create();
+
+        $response = $this->getJson('/api/v1/customers/' . $customer->id);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->where('data.id', $customer->id)
+                     ->where('data.name', $customer->name)
+                     ->where('data.postalCode', $customer->postal_code)
+                     ->etc()
+            );
     }
 }
