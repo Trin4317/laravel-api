7593f8876da94ab722f37fa984ac8c59e10e4b99
Trin4317 <eg.artourbabaev@gmail.com>
2023-02-23 14:56:31 +0000

    update invoice api test for index and show method

--- tests/Feature/InvoiceApiTest.php
+++ tests/Feature/InvoiceApiTest.php
@@ -2,18 +2,51 @@
 
 namespace Tests\Feature;
 
+use App\Models\Invoice;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class InvoiceApiTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_user_can_access_api(): void
+    public function test_user_can_get_all_invoices(): void
     {
+        $invoices = Invoice::factory(20)->create();
+
         $response = $this->getJson('/api/v1/invoices');
 
-        $response->assertStatus(200);
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->has('meta')
+                     ->has('links')
+                     ->has('data', 10)
+                     ->has('data.0', fn (AssertableJson $json) =>
+                        $json->where('id', $invoices[0]->id)
+                             ->where('customerId', $invoices[0]->customer->id)
+                             ->where('amount', $invoices[0]->amount)
+                             ->where('status', $invoices[0]->status)
+                             ->etc()
+                )
+            );
+    }
+
+    public function test_user_can_get_specific_invoice(): void
+    {
+        $invoice = Invoice::factory()->create();
+
+        $response = $this->getJson('/api/v1/invoices/' . $invoice->id);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->where('data.id', $invoice->id)
+                     ->where('data.customerId', $invoice->customer->id)
+                     ->where('data.amount', $invoice->amount)
+                     ->etc()
+            );
     }
 }
