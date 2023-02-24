3d65d798023eac5ecf10fc0ad970ec5a3a9f319e
Trin4317 <eg.artourbabaev@gmail.com>
2023-02-24 16:36:00 +0000

    add test cases for Customer Api

--- tests/Feature/CustomerApiTest.php
+++ tests/Feature/CustomerApiTest.php
@@ -33,6 +33,56 @@ class CustomerApiTest extends TestCase
             );
     }
 
+    public function test_user_can_filter_customers_with_one_condition(): void
+    {
+        Customer::factory(20)->create([
+            'postal_code' => '10000'
+        ]);
+
+        $customerWithBigPostalCode = Customer::factory()->create([
+            'postal_code' => '50000'
+        ]);
+
+        $response = $this->getJson('/api/v1/customers?postalCode[gt]=30000');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->has('meta')
+                     ->has('links')
+                     ->has('data', 1)
+                     ->has('data.0', fn (AssertableJson $json) =>
+                        $json->where('id', $customerWithBigPostalCode->id)
+                             ->where('name', $customerWithBigPostalCode->name)
+                             ->where('postalCode', $customerWithBigPostalCode->postal_code)
+                             ->etc()
+                )
+            );
+    }
+
+    public function test_user_can_filter_customers_with_many_conditions(): void
+    {
+        Customer::factory(20)->create([
+            'postal_code' => '10000',
+            'type' => 'I'
+        ]);
+
+        Customer::factory(2)->create([
+            'postal_code' => '50000',
+            'type' => 'B'
+        ]);
+
+        $response = $this->getJson('/api/v1/customers?postalCode[gt]=30000&type[eq]=B');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->has('meta')
+                     ->has('links')
+                     ->has('data', 2)
+            );
+    }
+
     public function test_user_can_get_specific_customer(): void
     {
         $customer = Customer::factory()->create();
