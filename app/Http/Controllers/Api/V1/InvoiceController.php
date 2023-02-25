849cb4a829d26d45921688f4f661a37e1e246e66
Trin4317 <eg.artourbabaev@gmail.com>
2023-02-25 12:10:21 +0000

    create bulkStore method in Invoice controller

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Requests\StoreInvoiceRequest;
-use App\Http\Requests\UpdateInvoiceRequest;
+use App\Http\Requests\V1\StoreInvoiceRequest;
+use App\Http\Requests\V1\BulkStoreInvoiceRequest;
+use App\Http\Requests\V1\UpdateInvoiceRequest;
 use App\Http\Controllers\Controller;
 use App\Models\Invoice;
 use App\Http\Resources\V1\InvoiceResource;
 use App\Http\Resources\V1\InvoiceCollection;
 use App\Filters\V1\InvoicesFilter;
+use Illuminate\Support\Arr;
 
 class InvoiceController extends Controller
 {
@@ -34,6 +36,20 @@ class InvoiceController extends Controller
     public function store(StoreInvoiceRequest $request)
     {
         //
+    }
+
+    /**
+     * Store multiple resources in storage.
+     */
+    public function bulkStore(BulkStoreInvoiceRequest $request)
+    {
+        // Note: Eloquent does not provide createMany method, so we use insert method instead
+        // Limitations: https://laraveldaily.com/post/eloquent-create-query-builder-insert
+        $bulk = collect($request->all())->map(function ($arr, $key) {
+            return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+        });
+
+        Invoice::insert($bulk->toArray());
     }
 
     /**
