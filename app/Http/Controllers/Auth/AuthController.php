f71f07630780282c2298526b65981ee374f0b83a
Trin4317 <eg.artourbabaev@gmail.com>
2023-02-26 13:23:59 +0000

    create login method in Auth controller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginUserRequest;
 use App\Http\Requests\Auth\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -20,6 +22,24 @@ class AuthController extends Controller
 
         return response()->json([
             'user'  => $user->makeHidden(['id', 'created_at', 'updated_at']),
+            'token' => $user->createToken('basic_token', ['none'])->plainTextToken
+        ]);
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        if (!Auth::attempt(
+                $request
+                 ->safe()
+                 ->only(['email', 'password'])
+            )) {
+            abort(401, 'Credentials do not match.');
+        }
+
+        $user = User::whereEmail($request->email)->first();
+
+        return response()->json([
+            'user'  => $user->setVisible(['name', 'email']),
             'token' => $user->createToken('basic_token', ['none'])->plainTextToken
         ]);
     }
